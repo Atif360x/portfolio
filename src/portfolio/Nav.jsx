028e3c5678b275079bb947d9f7999853bd64539f
Atif360x <mansuriatif78@gmail.com>
2026-07-08 21:39:50 +0530

    create nav bar

---
+++ src/portfolio/Nav.jsx
@@ -0,0 +1,24 @@
+function Nav () {
+    return (
+        <nav className="navbar">
+            <div className="nav_icn">
+            <div className="navicon icon">
+                <span className="material-symbols-outlined"> 
+                    code_xml
+                </span>
+            </div>
+            <div className="navicon">
+                <p>full stack devloper</p>
+            </div>
+            </div>
+           
+            <div className="navls">
+                <p>home</p>
+                <p>about</p>
+                <p>collabe</p>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
