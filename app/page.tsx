d895c471e176912d72c5cb9bdf29b5f6c1ddc03c
Atif <mansuriatif78@gmail.com>
2026-08-07 12:57:38 +0530

    made hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import { Inter, Roboto_Mono, Playfair_Display } from 'next/font/google';
 
 export default function Home() {
   return (
     <main>
-      
+      <div className=" h-[100vh] w-[100vw] bg-linear-160 from-[#000]/20 to-[75%] to-[#000]">
+
+        <h1 className="font-[playfair_Display] leading-none mix-blend-difference pl-5 pt-[80] text-white text-[60px] font-bold md:w-[60vw] md:text-[80px] lg:w-[60vw] lg:text-[100px]">
+          FULL STACK DEVELOPER
+        </h1>
+        <h2 className="font-[playfair_Display] flex justify-self-end text-right leading-none mix-blend-difference pt-[20vh] text-white text-[50px] font-bold md:w-[60vw] md:text-[70px] lg:w-[60vw] lg:text-[80px]">
+          making clean and heigh end web experince
+        </h2>
+
+        <div className="flex w-fill gap-5 justify-center pt-20">
+          <div className="text-white py-5 px-10 w-[40vw] h-[60] border-1 rounded-[999px] flex items-center justify-center text-center ">view work</div>
+          <div className="text-white py-5 px-10 w-[40vw] h-[60] border-1 rounded-[999px] flex items-center justify-center text-center ">connect</div>
+        </div>
+      </div>
+
+
     </main>
   );
 }
---
+++ components/Nav.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="fixed top-0 border-b-1 border-[#00FF00]/15 left-0 w-full h-[70px] z-50">
+      <div className="absolute inset-0 bg-black/20 backdrop-blur-md" />
+
+      <nav className="relative w-full h-full flex items-center justify-between px-5 text-white">
+        <div className="font-bold text-xl">
+          {'<ATIF />'}
+        </div>
+        
+        <div className="flex text-xl gap-2 md:gap-5 lg:gap-8">
+          <Link href="#work">work</Link>
+          <span>/</span>
+          <Link href="#about">about</Link>
+          <span>/</span>
+          <Link href="#connect">connect</Link>
+        </div>
+      </nav>
+
+    </div>
+  );
+}
